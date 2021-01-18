edf272a6f903f08a16cf7d8b32d54738e63c525e
yattrium <wolf.mikoto@gmail.com>
2021-01-18 15:58:02 +0300

    Комната(л. 3 и 4)

---
+++ room/Assets/Example.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    // Move object using accelerometer
+    float speed = 1.0f;
+
+    void Update()
+    {
+        Vector3 dir = Vector3.zero;
+
+        // we assume that device is held parallel to the ground
+        // and Home button is in the right hand
+
+        // remap device acceleration axis to game coordinates:
+        // 1) XY plane of the device is mapped onto XZ plane
+        // 2) rotated 90 degrees around Y axis
+        dir.x = -Input.acceleration.x;
+        dir.z = -Input.acceleration.y;
+
+        // clamp acceleration vector to unit sphere
+        if (dir.sqrMagnitude > 1)
+            dir.Normalize();
+
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        dir *= Time.deltaTime;
+
+        // Move object
+        if (Input.acceleration.x > 0.07 || Input.acceleration.x < -0.07)
+        {
+            transform.Translate(dir * speed);
+        }
+    }
+}
---
+++ room/Assets/playmove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class playmove : MonoBehaviour
+{
+    public Camera cam;
+     Animator animator;
+    private bool walking = false;
+    NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+        
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            walking = false;
+        }
+        else
+        {
+            walking = true;
+        }
+
+        animator.SetBool("bol", walking);
+        
+    }
+}
